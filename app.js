dde99f8385721c20b871441c0f82e7b97849b90c
sumit1203 <34911263+sumit1203@users.noreply.github.com>
2018-03-07 22:41:23 +0530

    cors added

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/api', api);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
